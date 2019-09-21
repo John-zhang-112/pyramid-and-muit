cd9723fbdc823ad5eb242f0e7631e9536c2d4fa4
John-zhang <54849353+John-zhang-112@users.noreply.github.com>
2019-09-21 20:45:32 +0800

    Update pyramid.java

--- pyramid.java
+++ pyramid.java
@@ -2,17 +2,20 @@ public class pyramid{
 	
 	public static void main(String[] args){
 		
-		int i,m=9,j,n;
+	int i,m=9,j,n;
 		
-	    for(i=1;i<=m;++i){
+	for(i=1;i<=m;++i){
 			
-			for(j=1;j<=m-i;j++){
-				System.out.print(" ");
-			for(n=1;n<=2*i-1;n++);
-			    System.out.print("$");
-			
-			System.out.println();
-			}
-		}
+	for(j=1;j<=m-i;j++){
+	 
+        System.out.print(" ");
+	 
+        for(n=1;n<=2*i-1;n++);
+	     
+	System.out.print("$");
+              
+	System.out.println();
 	}
+     }
+  }
 }
