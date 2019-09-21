ceadfe84e68ba0179933f1b458bf591c068a9e8f
John-zhang <54849353+John-zhang-112@users.noreply.github.com>
2019-09-21 20:41:45 +0800

    Update max.java

--- max.java
+++ max.java
@@ -1,13 +1,11 @@
 
  public class max{
 	public static void main(String[] args){
-		int i,j;
+	int i,j;
         for(i=1;i<=9;i++){
-       
-		for(j=1;j<=i;j++)
-			System.out.print(i+"*"+i+"="+(i*j)+" ");
-		
-		}
-		System.out.println();
-    }
+	  for(j=1;j<=i;j++)
+	     System.out.print(i+"*"+i+"="+(i*j)+" ");	
+	}
+	System.out.println();
+        }
 }
