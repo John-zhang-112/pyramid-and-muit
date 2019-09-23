e47b3237bc1a1519e7a27b3b34c5dfe1a59b0b31
John-zhang <54849353+John-zhang-112@users.noreply.github.com>
2019-09-23 21:09:58 +0800

    Update muit.java

--- muit.java
+++ muit.java
@@ -1,11 +1,19 @@
 
  public class muit{
+	
 	public static void main(String[] args){
+	
 	int i,j;
-        for(i=1;i<=9;i++){
-	  for(j=1;j<=i;j++)
-	     System.out.print(i+"*"+i+"="+(i*j)+" ");	
+        
+	for(i=1;i<=9;i++){
+	  
+	     for(j=1;j<=i;j++)
+	     
+             System.out.print(i+"*"+i+"="+(i*j)+" ");	
+	
 	}
+	
 	System.out.println();
-        }
+        
+	}
 }
